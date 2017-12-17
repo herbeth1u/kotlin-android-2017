768472f9001a4de7b2a73b1f0b9799969c7ebaa1
Thomas HERBETH <thomas.herbeth@gmail.com>
2017-12-17 14:37:34 +0100

    [#] Simplified Rx call with doFinally instead of doOnSuccess / doOnError / doOnDispose.

--- app/src/main/java/work/beltran/kotlinandroidmvp/ui/MainPresenter.kt
+++ app/src/main/java/work/beltran/kotlinandroidmvp/ui/MainPresenter.kt
@@ -6,7 +6,7 @@ import work.beltran.kotlinandroidmvp.api.Repo
 import work.beltran.kotlinandroidmvp.di.Schedulers
 import javax.inject.Inject
 
-class MainPresenter @Inject constructor(val interactor: GithubService, val schedulers: Schedulers) {
+class MainPresenter @Inject constructor(private val interactor: GithubService, private val schedulers: Schedulers) {
     private var view: MainView? = null
     private val composite = CompositeDisposable()
 
@@ -19,9 +19,7 @@ class MainPresenter @Inject constructor(val interactor: GithubService, val sched
                 .subscribeOn(schedulers.io())
                 .observeOn(schedulers.ui())
                 .doOnSubscribe { view?.showLoading(true) }
-                .doOnDispose { view?.showLoading(false) }
-                .doOnSuccess { view?.showLoading(false) }
-                .doOnError { view?.showLoading(false) }
+                .doFinally { view?.showLoading(false) }
                 .subscribe(this::onNext, this::onError))
     }
 
